e3e9a8f50f1d2c563167000119316692f9426887
kiranendarapu@gmail.com <kiranendarapu@gmail.com>
2026-03-04 21:16:27 +0530

    added the docker and docker compose file and changed the vite config file to run it on the docker localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true, // Needed for the Docker Container port mapping to work
+    port: 5173, // You can replace this port with any port
+    watch: {
+      usePolling: true // For file watching inside the container
+    }
+  },
+});
