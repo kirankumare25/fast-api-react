6b9fc9ce84c22459c7f27dea0ab41d683ca77cc0
kiranendarapu@gmail.com <kiranendarapu@gmail.com>
2026-03-04 20:49:22 +0530

    adding the docker files and docker compose

--- backend/main.py
+++ backend/main.py
@@ -78,4 +78,4 @@ def delete_user(user_id : int):
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
