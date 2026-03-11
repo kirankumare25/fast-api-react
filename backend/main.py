c4db607b348368b7023d13040a275dd6f0ab9839
kiranendarapu@gmail.com <kiranendarapu@gmail.com>
2026-03-11 16:35:00 +0530

    added database function to all the endpoints

--- backend/main.py
+++ backend/main.py
@@ -4,12 +4,20 @@ import redis.asyncio as redis
 from database import SessionLocal, engine, Base
 from typing import Annotated
 from sqlalchemy.orm import Session
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, func
 import uvicorn
 import json
-import os
+from pydantic import BaseModel
 
+class UserSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    job_title: str
 
+    class Config:
+        from_attributes = True 
 
 app = FastAPI()
 
@@ -48,6 +56,17 @@ class MockData(Base):
     ip_address = Column(String(50), nullable=False)
 
 
+class UserSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    job_title: str
+
+    class Config:
+        from_attributes = True
+
+
 async def custom_rate(
     request: Request,
     rlocal: redis.Redis = Depends(get_redis_data),
@@ -75,34 +94,27 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-users = []
 
-WORKDIR = os.getcwd()
-FILE = os.path.join(WORKDIR + "/database.json") #docker file path
-# FILE = os.path.join(WORKDIR + "/backend/database.json") #vs code file path
+# WORKDIR = os.getcwd()
+# FILE = os.path.join(WORKDIR + "/database.json") #docker file path
+# # FILE = os.path.join(WORKDIR + "/backend/database.json") #vs code file path
 
-with open(FILE, "r") as file:
-    data = json.load(file)
-    users = data
+# with open(FILE, "r") as file:
+#     data = json.load(file)
+#     users = data
 
 
 @app.get('/')
 def home():
     return {"message": "Hellow from Fast API"}
 
-@app.get('/getdb')
-def get_db_data(db : db_dependancy):
 
-    data = db.query(MockData).all()
-
-    return data
-
-@app.get("/rate", dependencies=[Depends(custom_rate)])
+@app.get("/ratelimter", dependencies=[Depends(custom_rate)])
 async def rate_limite_check():
     return {"message": "ok"}
 
 @app.get("/getdetails",dependencies=[Depends(custom_rate)])
-async def get_from_redis(id : int, cache : redis.Redis = Depends(get_redis_data)):
+async def get_from_redis(id : int,db : db_dependancy, cache : redis.Redis = Depends(get_redis_data)):
     cache_data = await cache.get(f"{id}")
     if cache_data:
         data = json.loads(cache_data)
@@ -111,43 +123,49 @@ async def get_from_redis(id : int, cache : redis.Redis = Depends(get_redis_data)
         jdata = json.dumps(data, indent=4)
         return Response(content=jdata, media_type="application/json")
     else:
-        for user in users:
-            if user["id"] == id:
-                await cache.set(f"{id}", json.dumps(user), ex=60)
-                user["source"] = "from database"
-                jdata = json.dumps(user, indent=4)
-                return Response(content=jdata, media_type="application/json")
-    
+        user = db.query(MockData).filter(MockData.id == id).first()
+        if user:
+            user_dict = UserSchema.model_validate(user).model_dump()
+            await cache.set(f"{id}", json.dumps(user_dict, indent=4), ex=60)
+            user_dict["source"] = "from database"
+            return Response(content=user_dict, media_type="application/json")
 
 @app.get('/user/details',dependencies=[Depends(custom_rate)])
-def user_details():
-    return Response(content=json.dumps(users, indent=4), media_type="application/json")
+def user_details(db : db_dependancy):
+    users = db.query(MockData).all()
+    models_out = [UserSchema.model_validate(u).model_dump() for u in users]
+    return Response(content=json.dumps(models_out, indent=4), media_type="application/json")
+
 
 @app.get('/user/details/{user_id}',dependencies=[Depends(custom_rate)])
-def user_details_id(user_id : int):
-    for user in users:
-        if user["id"] == user_id:
-            return Response(content=json.dumps(user, indent=4), media_type="application/json")
+def user_details_id(user_id : int, db: db_dependancy):
+    user = db.query(MockData).filter(MockData.id == user_id).first()
+    if user:
+        model_out = UserSchema.model_validate(user).model_dump()
+        return Response(content=json.dumps(model_out, indent=4), media_type="application/json")
         
     return {"message": f"{user_id} doesn't exist"}
 
 @app.get('/search',dependencies=[Depends(custom_rate)])
-def search_user(name : str):
-    for user in users:
-        if user["first_name"].lower() == name.lower():
-             return Response(content=json.dumps(user, indent=4), media_type="application/json")
+def search_user(name : str, db: db_dependancy):
+    user = db.query(MockData).filter(func.lower(MockData.first_name) == name.lower()).first()
+    if user:
+        model_out = UserSchema.model_validate(user).model_dump()
+        return Response(content=json.dumps(model_out, indent=4), media_type="application/json")
+    
     return {"message" : f"{name.capitalize()} has not found on database"} 
     
 @app.delete('/user/delete/{user_id}',dependencies=[Depends(custom_rate)])
-def delete_user(user_id : int):
-    for user in users:
-        if user_id == user["id"]:
-            users["users"].remove(user)
-            return {"message": "user has deleted"}
-           
-    return {"message": f"{user_id} doesn't exist"}
-            
-        
+def delete_user(user_id : int, db : db_dependancy):
+     user = db.query(MockData).filter(MockData.id == user_id).first()
+
+     if not user:
+         return {"message": f"{user_id} doesn't exist"}
+     
+     db.delete(user)
+     db.commit()
+     return {"message" : "user has deleted" }
+          
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
